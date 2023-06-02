b94b20df872aadd688e5f3581e72302071d3294d
DanielRodriguez-SS <83928934+DanielRodriguez-SS@users.noreply.github.com>
2023-06-02 16:06:10 +0930

    Update main.py

--- main.py
+++ main.py
@@ -3,5 +3,7 @@ import os
 from sklearn import svm
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-
+"""
+https://safjan.com/convert-heic-and-heif-to-jpg-png-bmp-with-python/
+"""
 # Main
