bb552c375ca2e79fab466bdf455612241227f763
DanielRodriguez-SS <83928934+DanielRodriguez-SS@users.noreply.github.com>
2023-06-09 15:44:44 +0930

    images convertor

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from PIL import Image
 import cv2
 import os
 from sklearn import svm
@@ -8,7 +9,15 @@ https://safjan.com/convert-heic-and-heif-to-jpg-png-bmp-with-python/
 """
 # Main
 
-print("Hola AI")
+#print("Hola AI")
 
 # By master god Daniel
-print('Python is cool')
+#print('Python is cool')
+
+def convert_heic_to_jpg(image_path:str):
+    outfile_name = image_path.split('.')[-1]
+    image = Image.open(image_path)
+    image.convert('RGB').save(f'{outfile_name}.jpg')
+
+if "__main__" == __name__:
+    convert_heic_to_jpg('images/with_ball/IMG_6104.HEIC')
